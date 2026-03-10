0faa81eaf682f7f156d3729ddbc3f094b1d0af2a
7CO <Hoangmen9918@gmail.com>
2026-03-10 21:52:01 +0700

    [FE] - 80% Completed

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           </div>
           <div id="nameSection" className="mb-1">
             <span
-              className={` ${cherryBomb.className} text-main text-stroke text-2xl`}
+              className={` ${cherryBomb.className} [-webkit-text-stroke:1px_black] text-main text-stroke text-2xl`}
             >
               Suw
             </span>
@@ -55,36 +55,141 @@ export default function Home() {
         </div>
 
         {/* Biography */}
-        <div id="bio" className="bg-secondary-gradient p-4 rounded-2xl border-2 w-[40%]">
+        <div
+          id="bio"
+          className="bg-secondary-gradient p-4 rounded-2xl border-3 w-[40%]"
+        >
           <h1
-            className={`text-main text-stroke text-lg ${cherryBomb.className}`}
+            className={`[-webkit-text-stroke:1px_black] flex items-center gap-1 text-main text-stroke text-xl ${cherryBomb.className}`}
           >
+            <Image
+              src="/icons/bio.png"
+              alt="Biography"
+              width={24}
+              height={24}
+            ></Image>
             BIOGRAPHY
           </h1>
           <p
-            className={`line-clamp-3 text-sm ${cherryBomb.className} text-stroke text-white`}
+            className={`[-webkit-text-stroke:1px_black] line-clamp-2 ${cherryBomb.className} text-stroke text-white`}
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mi
             arcu, tempus sit amzet luctus nec, consectetur at dolor.
           </p>
-          <br />
           <p
-            className={`line-clamp-2 text-sm ${cherryBomb.className} text-stroke text-white`}
+            className={`[-webkit-text-stroke:1px_black] line-clamp-2  ${cherryBomb.className} text-stroke text-white`}
           >
             Vivamus rutrum, felis in semper gravida, elit quam molestie massa,
             quis sollicitudin.
           </p>
-          <br />
           <p
-            className={`line-clamp-2 text-sm ${cherryBomb.className} text-stroke text-white `}
+            className={`[-webkit-text-stroke:1px_black] line-clamp-2  ${cherryBomb.className} text-stroke text-white `}
           >
             felis risus id urna. Maecenas quis euismod tellus. Orci varius
             natoque.
           </p>
         </div>
-        <div className="bg-secondary-gradient w-full p-4 rounded-2xl border-2"></div>
+        <div className="bg-secondary-gradient w-full p-4 rounded-2xl border-3"></div>
       </section>
-      <section id="information"></section>
+      <section id="information" className="mt-3 flex relative gap-2 ">
+        <div
+          id="additionalInformation"
+          className="bg-secondary-gradient p-4 rounded-2xl border-3 overflow-hidden w-[16%]"
+        >
+          <h1 className="text-main text-xl[-webkit-text-stroke:1px_black]">Additional:</h1>
+          <p className="[-webkit-text-stroke:1px_black] text-white  line-clamp-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <p className="[-webkit-text-stroke:1px_black] text-white  line-clamp-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+
+        </div>
+
+        <div id="personal" className="relative flex flex-col justify-between">
+          <div
+            id="lifeGoal"
+            className="p-2 bg-secondary-gradient rounded-2xl border-3 overflow-hidden w-48.75"
+          >
+            <div className="flex items-center gap-1">
+              <Image
+                src={"/icons/goal.png"}
+                alt="goals"
+                width={30}
+                height={30}
+              ></Image>
+              <span className="text-main text-xl [-webkit-text-stroke:1px_black]">Life goals:</span>
+            </div>
+            <ul className="text-white list-disc ">
+              <li className="mx-0 ml-6 [-webkit-text-stroke:1px_black]">To live happily with my family.</li>
+              <li className="mx-0 ml-6 [-webkit-text-stroke:1px_black]">To be Back-end dev.</li>
+            </ul>
+          </div>
+
+          <div
+            id="favoriteSeries "
+            className="bg-secondary-gradient p-4 rounded-2xl border-3 overflow-hidden w-48.75"
+          >
+            <span className="text-main text-xl [-webkit-text-stroke:1px_black]">Favorite Series:</span>
+            <ul className="text-white list-disc ">
+              <li className="mx-0 ml-4 [-webkit-text-stroke:1px_black]">Touhou Project.</li>
+              <li className="mx-0 ml-4 [-webkit-text-stroke:1px_black]">Albion Online.</li>
+              <li className="mx-0 ml-4 [-webkit-text-stroke:1px_black]">Blue Archive.</li>
+            </ul>
+          </div>
+        </div>
+        <div className=" bg-secondary-gradient p-4 rounded-2xl border-3 overflow-hidden w-2/4"></div>
+        <div
+          id="socialMedia"
+          className="relative flex flex-col items-center justify-between flex-1"
+        >
+          <a href="#" className="bg-secondary-gradient rounded-full border-3 flex items-center justify-center w-full py-2">
+            <span className="[-webkit-text-stroke:1px_black] flex items-center gap-2 text-main text-xl">
+              <Image
+                src="/icons/github.png"
+                alt="Github"
+                width={24}
+                height={24}
+              ></Image>
+              Github
+            </span>
+          </a>
+          <a href="#" className="bg-secondary-gradient rounded-full border-3 flex items-center justify-center w-full py-2">
+            <span className="[-webkit-text-stroke:1px_black] flex items-center gap-2 text-main text-xl">
+              <Image
+                src="/icons/x.png"
+                alt="Github"
+                width={24}
+                height={24}
+              ></Image>
+              X (Twitter)
+            </span>
+          </a>
+          <a href="" className="bg-secondary-gradient rounded-full border-3 flex items-center justify-center w-full py-2">
+            <span className="[-webkit-text-stroke:1px_black] flex items-center gap-2 text-main text-xl">
+              <Image
+                src="/icons/discord2.jpg"
+                alt="Github"
+                width={24}
+                height={24}
+                className="rounded"
+              ></Image>
+              Github
+            </span>
+          </a>
+          <a href="" className="bg-secondary-gradient rounded-full border-3 flex items-center justify-center w-full py-2">
+            <span className=" [-webkit-text-stroke:1px_black] flex items-center gap-2 text-main text-xl">
+              <Image
+                src="/icons/youtube.png"
+                alt="Github"
+                width={24}
+                height={24}
+              ></Image>
+              Youtube
+            </span>
+          </a>
+        </div>
+      </section>
     </main>
   );
 }
